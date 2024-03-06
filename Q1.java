85a53e414fcabaf223b495a89a533cdb0c49d306
JIBITESH KUMAR MISHRA <131876188+jibitesh2004@users.noreply.github.com>
2024-03-06 21:35:26 +0530

    Q1.java

--- Q1.java
+++ Q1.java
@@ -1,21 +1,27 @@
 import java.util.*;
-public class Q1 {
-	public static int repeat(int n) {
-		int m = n;
-		
-		
-		int count = 0;
-		while(n >2) {
-		   n/=2;
-		   count++;
-		}
-		return count;
-	}
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = repeat(a);
-		System.out.println(b);
-	}
+public class Q1{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number greater than 2 :- ");
+        while(true){
 
+        int num = sc.nextInt();
+        if(num >=2){
+            int count =0 ;
+            while(num>2){
+                num /=2;
+                count++;
+            }
+            System.out.println("The number of times the number will be divided is " + count);
+            break;
+
+        }
+        else{
+            System.out.println("You have entered number less than 2");
+            System.out.println("Plz try again:-");
+		}
+	}
 }
+}
+
+
